34d2ff4c0e01751f0831c5422495545996833a57
Reşat Yavçin <resatyavcin@icloud.com>
2025-09-21 18:12:41 +0300

    Add IGenericRepository interface for entity management
    Introduces the `IGenericRepository<TEntity>` interface in the `AuthServer.Core.Repositories` namespace. This interface implements a generic repository pattern, providing methods for asynchronous retrieval, querying, adding, updating, and removing entities of type `TEntity`. The `System.Linq.Expressions` namespace is imported to facilitate the use of expression trees in the `Where` method.

---
+++ AuthServer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace AuthServer.Core.Repositories
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity> GetByIdAsync(int id);
+        IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> predicate);
+        Task AddAsync(TEntity entity);
+        TEntity Update(TEntity entity);
+        void Remove(TEntity entity);
+
+    }
+}
