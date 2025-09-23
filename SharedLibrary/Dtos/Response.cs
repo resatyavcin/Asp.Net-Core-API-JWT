673e411a628d8bd578e249d8fdb1e59eb81f319f
Reşat Yavçin <resatyavcin@icloud.com>
2025-09-23 22:02:34 +0300

    Add ErrorDto class and enhance Response<T> handling
    Introduced a new `ErrorDto` class to encapsulate error information, including a list of error messages and a visibility flag. Updated the `Response<T>` class to include an `ErrorDto` property for better error response management. Added methods for creating successful and failed responses, with appropriate overloads. The `isSuccessFull` property is now marked with `[JsonIgnore]` to exclude it from JSON serialization.

---
+++ SharedLibrary/Dtos/Response.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SharedLibrary.Dtos
+{
+    public class Response<T> where T:class 
+    {
+        #region properties
+        public T Data { get; private set; }
+        public int StatusCode { get; private set; }
+
+        [JsonIgnore]
+        public bool isSuccessFull { get; set; }
+        public ErrorDto Error { get; private set; }
+        #endregion
+
+        #region methods
+        public static Response<T> Success(T data, int statusCode)
+        {
+            return new Response<T> { Data = data, StatusCode = statusCode, isSuccessFull = true };
+        }
+
+        public static Response<T> Success(int statusCode)
+        {
+            return new Response<T> { Data = default, StatusCode = statusCode, isSuccessFull = true };
+        }
+        public static Response<T> Fail(ErrorDto error, int statusCode, bool isShow)
+        {
+            return new Response<T> { Error = error, StatusCode = statusCode, isSuccessFull = false };
+        }
+        public static Response<T> Fail(string errorMessage, int statusCode, bool isShow)
+        {
+            var errorDto = new ErrorDto(errorMessage, isShow);
+            return new Response<T> { Error = errorDto, StatusCode = statusCode, isSuccessFull = false };
+        }
+        #endregion
+    }
+}
