bf7d0e3e3d20da52b7a068ba80bb79c7ee8c4eca
Reşat Yavçin <resatyavcin@icloud.com>
2025-09-24 21:59:12 +0300

    Add client and token DTOs with ITokenService updates
    Introduced new classes: `Client`, `ClientTokenDto`, and `TokenDto` to manage client configurations and token data. The `Client` class includes properties for `Id`, `Secret`, and `Audiences`. The token DTOs handle access tokens and their expiration, with `TokenDto` also supporting refresh tokens. Updated `ITokenService` interface to include methods for creating tokens for users and clients.

---
+++ AuthServer.Core/Services/ITokenService.cs
@@ -0,0 +1,12 @@
+﻿using AuthServer.Core.Configuration;
+using AuthServer.Core.DTOs;
+using AuthServer.Core.models;
+
+namespace AuthServer.Core.Services
+{
+    public interface ITokenService
+    {
+        TokenDto CreateToken(UserApp userApp);
+        ClientTokenDto CreateTokenByClient(Client client);
+    }
+}
