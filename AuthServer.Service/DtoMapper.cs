7d78ee9dd4f54ddd8a1fa030cf87a30fd74820a9
Reşat Yavçin <resatyavcin@icloud.com>
2025-09-27 22:43:42 +0300

    Add GenericRepository for Entity Framework Core
    Introduces a new `GenericRepository<TEntity>` class implementing the `IGenericRepository<TEntity>` interface. This class provides methods for adding, retrieving, updating, and removing entities, as well as querying with predicates. The repository pattern is used to abstract data access, enhancing manageability and testability of database interactions.
    
    Add AutoMapper and implement DTOs and repositories
    
    - Updated `AuthServer.Service.csproj` to include `AutoMapper` v10.1.1.
    - Added `ProductDto` class for data transfer with properties.
    - Introduced `UnitOfWork` class for database commit operations.
    - Created `GenericRepository<TEntity>` for CRUD operations.
    - Implemented `DtoMapper` for mapping between DTOs and models.
    - Added `ObjectMapper` for lazy initialization of AutoMapper.

---
+++ AuthServer.Service/DtoMapper.cs
@@ -0,0 +1,16 @@
+﻿using AuthServer.Core.DTOs;
+using AuthServer.Core.models;
+using AutoMapper;
+
+
+namespace AuthServer.Service
+{
+    class DtoMapper: Profile
+    {
+        public DtoMapper()
+        {
+            CreateMap<ProductDto, Product>().ReverseMap();
+            CreateMap<UserAppDto, UserApp>().ReverseMap();
+        }
+    }
+}
